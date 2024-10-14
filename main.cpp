4675a56b4660327ee64271a8de4920ca99bfee17
Dekta92 <delt4029@gmail.com>
2024-10-14 20:26:08 +0500

    Records keyboard and pointer data to custom file now

--- main.cpp
+++ main.cpp
@@ -1,13 +1,31 @@
 #include <X11/Xlib.h>
 #include <iostream>
 #include <unistd.h>
+#include <cstring>
 #include <ctime>
+#include <fstream>
+#include <sstream>
+#include <string>
 
-int Recorder(){
+struct PointerData{
+    short int Xcoord;
+    short int Ycoord;
+    bool LMB;
+    bool MMB;
+    bool RMB;
+    bool scrollUp;
+    bool scrollDown;
+};
+
+struct KeyboardData{
+    short int counter;
+    char keysArray[50][32];
+};
+
+int Recorder(std::string filename){
     Display* display;
     Window rootWindow;
     int rootX, rootY;
-    int oldrootX, oldrootY;
     int winX, winY;
     XEvent event;
     unsigned int mask;
@@ -22,77 +40,86 @@ int Recorder(){
     }
     rootWindow = DefaultRootWindow(display); // Initializes the window variable with the root window.
 
-    // Intialize Pointer Array for storing data
-    struct PointerData{
-        short int Xcoord;
-        short int Ycoord;
-        Bool LMB;
-        Bool MMB;
-        Bool RMB;
-        Bool scrollUp;
-        Bool scrollDown;
-    };
+    // Initialize PointerData
     PointerData Pointer[50];
-    XQueryPointer(display, rootWindow, &returnedRoot, &returnedChild, &rootX, &rootY, &winX, &winY, &mask);
-    Pointer[0].Xcoord = rootX;
-    Pointer[0].Ycoord = rootY;
-    Pointer[0].LMB = False;
-    Pointer[0].MMB = False;
-    Pointer[0].RMB = False;
-    Pointer[0].scrollUp = False;
-    Pointer[0].scrollDown = False;
+    for (int i = 0; i < 50; ++i){Pointer[i] = {0, 0, false, false, false, false, false};}
 
-    char keysArray[32];
+    // Initialize KeyboardData
+    KeyboardData keyboardData = {0};
+    char tempKeysArray[32] = {};
 
     int timeDelay = 50000; // in microseconds (10^-6 seconds)
-    int counter = 1;
+    int counter = 0;
     while (true) {
-        // XQueryPointer will initialize all the parameters it takes except display and rootWindow
-        // winX and winY will be same as rootX and rootY because the root window is passed to XQueryPointer()
+        // Get pointer and keyboard data
         XQueryPointer(display, rootWindow, &returnedRoot, &returnedChild, &rootX, &rootY, &winX, &winY, &mask);
-        XQueryKeymap(display, keysArray);
+        XQueryKeymap(display, tempKeysArray);
 
+        // Write pointer data to array
         std::cout << "Pointer is at: (" << rootX << ", " << rootY << ")"<<std::endl;
-        if(mask & Button1Mask){
-            Pointer[counter].LMB = True;
+        Pointer[counter].Xcoord = rootX; 
+        Pointer[counter].Ycoord = rootY;
+        Pointer[counter].LMB = (mask & Button1Mask) != 0;
+        Pointer[counter].MMB = (mask & Button2Mask) != 0;
+        Pointer[counter].RMB = (mask & Button3Mask) != 0;
+        Pointer[counter].scrollUp = (mask & Button4Mask) != 0;
+        Pointer[counter].scrollDown = (mask & Button5Mask) != 0;
+        
+        // Write keyboard data to array
+        std::strncpy(keyboardData.keysArray[keyboardData.counter % 50], tempKeysArray, 32);
+        keyboardData.keysArray[keyboardData.counter % 50][31] = '\0';
+
+        if ((counter + 1) % 50 == 0) {
+            std::ofstream outFile(filename, std::ios::app);
+            if (!outFile) {
+                std::cerr << "Error opening file!\n";
+                break;
+            }
+            for (int i = 0; i < 50; ++i) {
+                outFile << Pointer[i].Xcoord << ", " 
+                        << Pointer[i].Ycoord << ", " 
+                        << Pointer[i].LMB << ", " 
+                        << Pointer[i].MMB << ", " 
+                        << Pointer[i].RMB << ", " 
+                        << Pointer[i].scrollUp << ", " 
+                        << Pointer[i].scrollDown << "\n";
+                outFile << keyboardData.keysArray[i % 50] << "\n";
+            }
+            
+            outFile.close();
         }
-        if(mask & Button2Mask){
-            Pointer[counter].MMB = True;
-        }
-        if(mask & Button3Mask){
-            Pointer[counter].RMB = True;
-        }
-        if(mask & Button4Mask){
-            Pointer[counter].scrollUp = True;
-        }
-        if(mask & Button5Mask){
-            Pointer[counter].scrollDown = True;
-        }
+
 
         usleep(timeDelay);
         counter += 1;
+        counter = counter % 50;
+        
     }
     XCloseDisplay(display);
+    return 0;
 }
 
 
 int main() {
-    std::cout << "########################### Macro by Dekta92 on Github ###########################\n";
-    std::cout << "(Works only on X11!)\n\n";
-        
-    bool programContinue = True;
+    std::cout << "########################### Macro by Dekta92 on Github ###########################\n\n";
+
+    std::string filename = "";
+    bool programContinue = true;
     int choice = 0;
     while(programContinue){
-        std::cout << "Please select your choice (Press 1/2)\n";
-        std::cout << "1. Record\n";
-        std::cout << "2. Exit\n";
+        std::cout << "Please select your choice (Press 1/2)\n"
+                  << "1. Record\n"
+                  << "2. Exit\n";
         std::cin >> choice;
         switch (choice)
         {
         case 1:
-            Recorder();
+            std::cout << "Please input a name for your recording file\n";
+            std::cin >> filename;
+            filename += ".txt";
+            Recorder(filename);
             break;
-        
+
         case 2:
             programContinue = False;
         }
